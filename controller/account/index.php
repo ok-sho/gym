b99f9ee851e1565460343f852b3f0bcab5b19099
anikets033 <aniketsandhu9@gmail.com>
2026-07-17 11:53:02 -0700

    Update index.php

--- controller/account/index.php
+++ controller/account/index.php
@@ -1,4 +1,29 @@
-<?php 
+<?php
+
 redirect_if_not_logged_in();
-view('account/index.view.php', ['heading' => 'Account Info']);
-?>
+
+$db = $container->resolve('Core\Database');
+
+$user = $db->getOne(
+    "SELECT 
+        users.id,
+        users.given_name,
+        users.family_name,
+        users.email,
+        users.created_at,
+        membership_tiers.title AS membership_title
+     FROM users
+     LEFT JOIN membership_tiers
+        ON users.membership_tier_id = membership_tiers.id
+     WHERE users.email = :email",
+    [
+        'email' => $_SESSION['user']['email']
+    ]
+);
+
+authorize($user, 'Account could not be found.');
+
+view('account/index.view.php', [
+    'heading' => 'Account Information',
+    'user' => $user
+]);
